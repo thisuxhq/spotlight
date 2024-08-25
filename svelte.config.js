84ac44e895e65261c3bf007eeaf0512a4b0b0fd7
SpikeySanju <spikeysanju98@gmail.com>
2024-08-25 09:59:45 +0530

    remove deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-// import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,20 +11,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		// adapter: adapter({
-		// 	// See below for an explanation of these options
-		// 	routes: {
-		// 		include: ['/*'],
-		// 		exclude: ['<all>']
-		// 	},
-		// 	platformProxy: {
-		// 		configPath: 'wrangler.toml',
-		// 		environment: undefined,
-		// 		experimentalJsonConfig: false,
-		// 		persist: false
-		// 	}
-		// }),
-		adapter: adapter()
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		}),
 	}
 };
 
